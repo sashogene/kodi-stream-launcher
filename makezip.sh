142cbc76d4c898c235cb26375fbde88c7388c9ca
Alexander Georgiev <ageorgiev5@gmail.com>
2026-06-16 22:21:49 +0200

    Add makezip.sh script to automate packaging of the Kodi stream launcher

---
+++ makezip.sh
@@ -0,0 +1,3 @@
+!#/bin/bash
+cd ..
+zip kodi-stream-launcher.zip kodi-stream-launcher/addon.xml kodi-stream-launcher/resources/settings.xml kodi-stream-launcher/icon.png kodi-stream-launcher/default.py 
