40c96f0462e9e2ed743751e6db79937bd07b2017
Alexander Georgiev <ageorgiev5@gmail.com>
2026-06-19 12:01:15 +0200

    Fix addon ID and update metadata summary and description in addon.xml; correct shebang in makezip.sh

--- makezip.sh
+++ makezip.sh
@@ -1,5 +1,4 @@
-!#/bin/bash
-
+#!/bin/bash
 # Configurable list of files to include in the zip
 ADDON_FILES_TO_ZIP=(
     "kodi-stream-launcher/addon.xml"
@@ -7,7 +6,6 @@ ADDON_FILES_TO_ZIP=(
     "kodi-stream-launcher/icon.png"
     "kodi-stream-launcher/default.py"
 )
-
 cd ..
 rm kodi-stream-launcher.zip
 zip kodi-stream-launcher.zip "${ADDON_FILES_TO_ZIP[@]}" 
