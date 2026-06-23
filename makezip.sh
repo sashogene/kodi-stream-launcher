fb2f204a24cc4b5913b004706b3f434a6e000215
Alexander Georgiev <ageorgiev5@gmail.com>
2026-06-23 22:11:45 +0200

    Fixed paths

--- makezip.sh
+++ makezip.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Configurable list of files to include in the zip
 ADDON_FILES_TO_ZIP=(
-    "kodi-stream-launcher/addon.xml"
-    "kodi-stream-launcher/resources/settings.xml"
-    "kodi-stream-launcher/Kodi_launcher.png"
-    "kodi-stream-launcher/default.py"
+    "addon.xml"
+    "resources/settings.xml"
+    "Kodi_launcher.png"
+    "default.py"
 )
-#cd release
 rm release/kodi-stream-launcher.zip
 zip release/kodi-stream-launcher.zip "${ADDON_FILES_TO_ZIP[@]}" 
 cp release/kodi-stream-launcher.zip /nfs/Public/Kodi_development/
