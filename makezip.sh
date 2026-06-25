3be9363a33941fbed86d5e17e60af96a565be78f
Alexander Georgiev <ageorgiev5@gmail.com>
2026-06-25 18:36:54 +0200

    Refactor makezip.sh to correct file paths for inclusion in zip

--- makezip.sh
+++ makezip.sh
@@ -2,13 +2,13 @@
 # Configurable list of files to include in the zip
 ADDON_FILES_TO_ZIP=(
     "kodi-stream-launcher/addon.xml"
+    "kodi-stream-launcher/default.py"
+    "kodi-stream-launcher/Kodi_launcher.png"
     "kodi-stream-launcher/resources/settings.xml"
     "kodi-stream-launcher/resources/icon_viki.png"
     "kodi-stream-launcher/resources/icon_netflix.png"
     "kodi-stream-launcher/resources/icon_youtube.png"
     "kodi-stream-launcher/resources/icon_hbo_max.png"
-    "kodi-stream-launcher/resources/Kodi_launcher.png"
-    "kodi-stream-launcher/resources/default.py"
 )
 cd ..
 rm kodi-stream-launcher/release/kodi-stream-launcher.zip
